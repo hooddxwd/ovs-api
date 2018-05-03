f3ac9342b41ac41d3fafceab4d621cb68e66b8fa
Whoolie <marek.sobe@gmail.com>
2018-05-03 15:27:31 +0200

    run the test.py in sudo -s

--- ovs/test.py
+++ ovs/test.py
@@ -2,6 +2,12 @@ from flow import FlowTable
 flows = []
 br_name = "s1"
 table = FlowTable()
+sum = 0
 flows = table.dump_flow(br_name, flow_cond = None)
 for e in flows:
-	print e
+	#print e
+	if 'tp_src' in e: 
+		#print 'port: ' + e['tp_src']
+		if e['tp_src'] == '5692':
+			sum = sum + int(e['n_packets'])
+print sum
